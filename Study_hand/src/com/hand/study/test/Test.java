0fa56f1e2697cab27a51aea6a9ea4250420c7d38
HongYanT <407768418@qq.com>
2017-06-16 11:53:20 +0800

    "修改date"

--- Study_hand/src/com/hand/study/test/Test.java
+++ Study_hand/src/com/hand/study/test/Test.java
@@ -1,7 +1,10 @@
 package com.hand.study.test;
 
+import java.util.Date;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD");
+		Date date=new Date();
 	}
 }
