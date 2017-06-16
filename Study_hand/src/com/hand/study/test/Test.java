7e344f4e2127b9438e5480cd69411ce0dc886ac2
HongYanT <407768418@qq.com>
2017-06-16 11:54:25 +0800

    "注释date"

--- Study_hand/src/com/hand/study/test/Test.java
+++ Study_hand/src/com/hand/study/test/Test.java
@@ -5,6 +5,6 @@ import java.util.Date;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD");
-		Date date=new Date();
+		//Date date=new Date();
 	}
 }
